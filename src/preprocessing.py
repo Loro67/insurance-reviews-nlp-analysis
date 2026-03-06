f5ef997f518efcf4d7afaba90ff165dfb87d607c
Loro67 <leomanwinter@gmail.com>
2026-03-06 15:35:30 +0100

    data exploration

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import pandas as pd
+
+
 CURRENT_DIR = Path(__file__).resolve().parent
 DATA_PATH = CURRENT_DIR.parent / "Traduction_avis_clients"
-
 def read_dataset(n:int=35):
     dataset_list = []
     for i in range(1,n + 1):
@@ -20,3 +21,11 @@ def fusion_dataset(dataset_list: list[pd.DataFrame])-> pd.DataFrame:
     except Exception as e:
         print(f"Error  : {e}")
         return pd.DataFrame()
+
+WHITELIST = ""
+def spelling_correction(text: str) -> str:
+    # 1. Minuscules & Nettoyage ponctuation
+    # 2. Correction orthographique (ex: 'assurence' -> 'assurance')
+    # 3. Retrait des stopwords (ex: 'le', 'un')
+    clean_text = ""
+    return clean_text
